ce711a4a6e207108db5e91382de3a49a52d29a09
gactocat <gactocat@gmail.com>
2019-05-24 10:09:39 +0900

    🚨 fix test case

--- infrastructure/src/test/kotlin/MovieTest.kt
+++ infrastructure/src/test/kotlin/MovieTest.kt
@@ -4,21 +4,25 @@ import entity.OnairStatus
 import io.ktor.application.Application
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import io.ktor.util.KtorExperimentalAPI
 import org.assertj.core.api.Assertions
 import org.junit.Test
 import org.koin.standalone.StandAloneContext
 
+@KtorExperimentalLocationsAPI
+@KtorExperimentalAPI
 class MovieTest {
+    val objectMapper = jacksonObjectMapper()
 
     @Test
-    fun 動画を1件取得する() {
+    fun `Get a movie when request by id`() {
         withTestApplication(Application::main) {
-            with(handleRequest(HttpMethod.Get, "v1/movies/111")) {
-                HttpStatusCode.OK
+            with(handleRequest(HttpMethod.Get, "v1/movies/1")) {
                 Assertions.assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-                val movie = jacksonObjectMapper().readValue(response.content, MovieResponse::class.java)
+                val movie = objectMapper.readValue(response.content, MovieResponse::class.java)
                 Assertions.assertThat(movie).isEqualTo(MovieResponse(
                         1, "Test", OnairStatus.IN_LIVE.rawValue
                 ))
